b7a8e95c802e430128215d94777a936a3df9bc88
maverickalo <sean.vernon88@gmail.com>
2018-05-05 09:18:17 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://seanv:shasta2550@ds117749.mlab.com:17749/devconnector-sean'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hey World'));
+
+//USE ROUTES
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+const port = process.env.Port || 5000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
